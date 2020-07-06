19eafe6a9406f98a25942bf39d224410cf30bf8d
WilliamCappelletti <cappelletti.william@gmail.com>
2020-07-06 12:35:49 +0800

    change softmax to numerically stable version

--- polyadicqml/quantumClassifier.py
+++ polyadicqml/quantumClassifier.py
@@ -10,9 +10,12 @@ import datetime
 
 from scipy.optimize import minimize
 from sklearn.metrics import log_loss
-from scipy.special import softmax
 
 from .circuitML import circuitML
+
+def stable_softmax(X, axis=None):
+    exps = np.exp(X - np.max(X, axis=axis).reshape(-1,1))
+    return exps / np.sum(exps, axis=axis).reshape(-1,1)
 
 def CE_loss(y_true, y_pred, labels=None):
     """Cross entropy loss.
@@ -38,7 +41,7 @@ def CE_loss(y_true, y_pred, labels=None):
     # labels=np.concatenate((labels, [len(labels)]))
     # _y = np.hstack((y_pred, (1 - y_pred.sum(axis=1)).reshape(-1,1)))
 
-    return log_loss(y_true, softmax(y_pred, axis=1), labels=labels)
+    return log_loss(y_true, stable_softmax(y_pred, axis=1), labels=labels)
 
 def CE_grad(y_true, y_pred):
     """Cross entropy loss gradient, w.r.t. y_pred columns.
@@ -58,7 +61,7 @@ def CE_grad(y_true, y_pred):
 
     n = len(y_pred)
 
-    sigma = softmax(y_pred, axis=1)
+    sigma = stable_softmax(y_pred, axis=1)
     sigma[range(n), y_true] -= 1
     return sigma.sum(axis=0) / n
 
