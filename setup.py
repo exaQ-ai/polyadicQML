0a6790749a3a7835e5787d59fca7a8124753f4b2
WilliamCappelletti <cappelletti.william@gmail.com>
2020-07-14 17:15:19 +0800

    Pre-release version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="polyadicqml",
-    version="0.1.0.dev0",
+    version="0.1.0.dev1",
     author="William Cappelletti",
     author_email="cappelletti.william@gmail.com",
     description="Poliadic Quantum Machine Learning",
