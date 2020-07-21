add gradient to model, fix rest to comply

polyadicqml/circuitML.py
@@ -1,6 +1,6 @@
 """Implementation of circuit for ML
 """
-from numpy import pi, random, zeros_like, zeros, log2
+import numpy as np
 
 
 class circuitML():
@@ -79,8 +79,8 @@ class circuitML():
         vector
             Vector of random parameters.
         """
-        if seed: random.seed(seed)
-        return random.randn(self.nbparams)
+        if seed: np.random.seed(seed)
+        return np.random.randn(self.nbparams)
 
     def make_circuit(self, bdr, x, params):
         """Generate the circuit corresponding to input `x` and `params`.
@@ -149,36 +149,37 @@ class circuitML():
             2**nbqbits)* if `v` is None, else Jacobian-vector product:
             ``J(circuit) @ v``.
         """
-        if order > 2: raise NotImplementedError
+        if order > 2:
+            raise NotImplementedError
 
         if len(X.shape) < 2:
             X = X.reshape(1, -1)
         N, *_ = X.shape
         dim_out = (N, 2**self.nbqbits)
         if v is not None:
-            if len(v.shape) > 1:
-                dim_out = (v.shape[-1],)
-            else:
-                dim_out = (1,)
+            # if len(v.shape) > 1:
+            #     dim_out = (v.shape[-1],)
+            # else:
+            dim_out = (1,)
 
         if eps is None:
             if nbshots is None:
                 eps = 1e-8
             else:
                 eps = max(
-                    log2(self.nbqbits)*2*pi/3 * min(.5, 1/nbshots**.25),
+                    np.log2(self.nbqbits)*2*np.pi/3 * min(.5, 1/nbshots**.25),
                     1e-8
                 )
 
         num = eps if nbshots is None else eps * nbshots
 
-        out = zeros((self.nbparams,) + dim_out)
+        out = np.zeros((self.nbparams,) + dim_out)
 
         run_out = 0
         if order == 1:
             run_out = self.run(X, params, nbshots, job_size) / num
 
-        d = zeros_like(params)
+        d = np.zeros_like(params)
         for i in range(len(params)):
             d.fill(0)
             d[i] = eps
@@ -189,6 +190,6 @@ class circuitML():
                 pd = (self.run(X, params + d, nbshots, job_size) -
                       self.run(X, params - d, nbshots, job_size)) / num / 2
 
-            out[i] = pd if v is None else pd @ v
+            out[i] = pd if v is None else np.sum(pd * v)
 
         return out

polyadicqml/quantumClassifier.py
@@ -10,7 +10,7 @@ from scipy.optimize import minimize
 
 from .circuitML import circuitML
 
-from .utility import CE_loss
+from .utility import CE_loss, CE_grad
 
 SCIPY_METHODS = {
     'bfgs', 'nelder-mead', 'powell', 'cg',
@@ -79,6 +79,10 @@ class Classifier():
         nbshots_increment = kwargs.get('nbshots_increment')
         nbshots_incr_delay = kwargs.get('nbshots_incr_delay')
         loss = kwargs.get('loss', CE_loss)
+        loss_grad = kwargs.get(
+            'loss_grad',
+            CE_grad if loss is CE_loss else None
+        )
         job_size = kwargs.get('job_size')
         budget = kwargs.get('budget', 100)
         name = kwargs.get('name')
@@ -121,6 +125,7 @@ class Classifier():
         self.job_size = job_size
 
         self.__loss__ = loss
+        self.__loss_grad__ = loss_grad
         self.__min_loss__ = np.inf
 
         self.__last_loss_value__ = None
@@ -286,6 +291,26 @@ class Classifier():
             out = out / float(self.nbshots)
 
         return out[:, self.bitstr]
+
+    def grad(self, X, y, params):
+        # Compute the Jacobian vector product of
+        # Jac_{params}(circuit) @ Grad_{bistr}(loss)
+
+        if len(X.shape) < 2:
+            X = X.reshape(1, -1)
+        N, *_ = X.shape
+
+        v = np.zeros((N, 2**self.circuit.nbqbits))
+        v[:, self.bitstr] = self.__loss_grad__(
+            y, self.predict_proba(X, params)
+        )[:, :, 0]
+
+        return self.circuit.grad(
+            X, params,
+            v=v,
+            nbshots=self.nbshots,
+            job_size=self.job_size,
+        )
 
     def proba_to_label(self, proba) -> np.ndarray:
         """Transforms a matrix of real values in integer labels.

polyadicqml/utility.py
@@ -49,14 +49,14 @@ def CE_grad(y_true, y_pred):
     Returns
     -------
     ndarray
-        Jacobian of the loss, of size *(n_samples, n_classes)*.
+        Jacobian of the loss, of size *(n_samples, n_classes, 1)*.
     """
 
     n = len(y_pred)
 
     sigma = stable_softmax(y_pred, axis=1)
     sigma[range(n), y_true] -= 1
-    return sigma / n
+    return (sigma / n)[:, :, np.newaxis]
 
 
 def print_results(target, pred, name=None, output=None):