7a10b2c64bff4d94409705e70afef73764e916cb
WilliamCappelletti <cappelletti.william@gmail.com>
2020-07-24 00:03:24 +0800

    Set version to beta to beta4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup_kwargs = dict(
     name="polyadicqml",
-    version="0.1.0b3",
+    version="0.1.0b4",
     author="William Cappelletti",
     author_email="cappelletti.william@gmail.com",
     description="High level API to define, train and deploy Polyadic Quantum Machine Learning models",
